461c4c95e2b7ed2638dcfeab7b9f4e443bd0b7fc
kian-fotovat <kianf2011@gmail.com>
2025-10-05 20:25:39 -0500

    Make the same update check fix in update.py

--- update.py
+++ update.py
@@ -114,7 +114,10 @@ def check_bot_updates(git_bin: str, branch_name: str) -> Optional[Tuple[str, str
     """Attempt a dry-run with git fetch to detect updates on remote."""
     try:
         updates = (
-            subprocess.check_output([git_bin, "fetch", "--dry-run"])
+            subprocess.check_output(
+                [git_bin, "fetch", "--dry-run"],
+                stderr=subprocess.STDOUT
+            )
             .decode("utf8")
             .split("\n")
         )
